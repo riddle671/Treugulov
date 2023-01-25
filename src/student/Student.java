1b1b90dfbb5ed0451192733fe9f7171028bd4816
riddle671 <riddle671@yandex.ru>
2023-01-25 16:22:05 +0300

    Tregulov13

--- src/student/Student.java
+++ src/student/Student.java
@@ -1,7 +1,40 @@
 package student;
 
 public class Student {
-    public static void main(String[] args) {
-
+    @Override
+    public String toString() {
+        return "Student{" +
+                "nombel=" + nombel +
+                ", name='" + name + '\'' +
+                ", famil='" + famil + '\'' +
+                ", yearstudy=" + yearstudy +
+                ", matem=" + matem +
+                ", ekon=" + ekon +
+                ", inyaz=" + inyaz +
+                '}';
     }
-}
+
+    public Student(int nombel, String name, String famil, int yearstudy, double matem, double ekon, double inyaz) {
+        this.nombel = nombel;
+        this.name = name;
+        this.famil = famil;
+        this.yearstudy = yearstudy;
+        this.matem = matem;
+        this.ekon = ekon;
+        this.inyaz = inyaz;
+    }
+
+    int nombel;
+        String name;
+        String famil;
+        int yearstudy;
+        double matem;
+        double ekon;
+        double inyaz;
+
+        }
+
+
+
+
+
--- src/student/StudentTest.java
+++ src/student/StudentTest.java
@@ -1,4 +1,15 @@
 package student;
 
+import java.util.Arrays;
+
 public class StudentTest {
+    public static void main(String[] args) {
+        Student st1 = new Student(1,"Ivan", "Kurt", 1992, 6.6,5.4,3.6);
+        Student st2 = new Student(2, "Tolya", "Parteev", 1990, 4.5,5.7,8.5);
+        Student st3 = new Student(3, "Irina","Olegrova", 1995, 8.6,8.6,9.7);
+
+        System.out.println("sredarif Ivan " + (st1.matem + st1.ekon + st1.inyaz)/3);
+        System.out.println("sredarif Tolya " + (st2.matem + st2.ekon + st2.inyaz)/3);
+        System.out.println("sredarif Irina " + (st3.matem + st3.ekon + st3.inyaz)/3);
+    }
 }
